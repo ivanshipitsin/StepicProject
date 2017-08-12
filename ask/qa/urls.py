f787a6a2f4cf50dc6e4fcf0b2d66007550ad3b4c
ivanshipitsin <ivanshipitsin200229@gmail.com>
2017-08-12 14:33:42 +0500

    FirstDjango

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 urlpatterns =[
-    url(r'^',qa.views.test,name='test'),
+    url(r'^$',qa.views.test,name='test'),
     url(r'^login/',qa.views.test,name='login'),
     url(r'^signup/',qa.views.test,name='signup'),
     url(r'^question/(?P<pk>\d+)/$',qa.views.test,name='question'),
