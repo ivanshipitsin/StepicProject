f0aa7ec59dda6888d7255882cc20d7a4e5c3a898
ivanshipitsin <ivanshipitsin200229@gmail.com>
2017-08-25 12:31:23 +0500

    P5

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
-from qa.views import test, news, popular, question
+from qa.views import test, news, popular, question, Ask
 urlpatterns =[
     url(r'^$',news,name='test'),
     url(r'^login/',test,name='login'),
     url(r'^signup/',test,name='signup'),
     url(r'^question/(?P<pk>\d+)/$',question,name='question'),
-    url(r'^ask/',test,name='ask'),
+    url(r'^ask/',Ask,name='ask'),
     url(r'^popular/',popular,name='popular'),
     url(r'^new/',test,name='new')
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Question, Answer
 from django.core.exceptions import ObjectDoesNotExist
+from .forms import AskForm, AnswerForm
 
 def test(request,*args,**kwargs):
     return HttpResponse('OK')
@@ -37,6 +38,31 @@ def popular(request):
     return render(request,"index.html",{"page":contacts,"paginator":p})
 
 def question(request,pk):
-    qa = get_object_or_404(Question,id=pk)
-    ans= Answer.objects.filter(question_id__exact=int(pk))
+    if request.method == 'POST':
+        return answer(request)
+    else:
+        qa = get_object_or_404(Question,id=pk)
+        ans= qa.answer_set.all()
     return render(request,'question.html',{'question':qa,'answer':ans})
+
+def Ask(request):
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            url=post.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AskForm()
+    return render(request,'ask.html',{'form':form})
+
+def Answer(request):
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save()
+            url = answer.question.get_url()
+            return HttpResponseRedirect(url)
+    else:
+        form = AnswerForm()
+    return render(request,'answer.html',{'form':form})
